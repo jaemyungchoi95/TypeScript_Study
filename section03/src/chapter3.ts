e3093a16157280ec05929a522113e44a9bd337ab
jaemyungchoi95 <woaud130@gmail.com>
2026-02-23 14:45:14 +0900

    back up

---
+++ section03/src/chapter3.ts
@@ -0,0 +1,55 @@
+/**
+ * Unkwown 타입 -> 타입스크립트에 존재하는 모든 타입들의 슈퍼타입이다.
+ */
+
+function unknownExam(){
+    let a : unknown = 1;
+    let c : unknown = "hello";
+    let b : unknown = true;
+    let d : unknown = null;
+    let e : unknown = undefined;
+
+    let unknownVar: unknown;
+
+    // 다운캐스팅들은 전부 에러 발생
+    // let num: number = unknownVar; // 에러 발생
+    // let str: string = unknownVar; // 에러 발생
+    // let bool: boolean = unknownVar; // 에러 발생
+    
+}
+
+/**
+ * Never 타입 -> 공집합
+ * 모든 타입(집합)의 서브타입이다.
+ */
+
+function neverExam(){
+    function neverFunc(): never {
+        while(true){
+        }
+    }
+
+    let num: number = neverFunc(); // 가능
+    let str: string = neverFunc(); // 가능
+    let bool: boolean = neverFunc(); // 가능
+
+    // never 타입에 다른 타입 변수들을 넣으면 모두 에러 발생 (다운캐스팅이기 때문)
+    // let never1: never = 10;
+    // let never2: never = "hello";
+    // let never3: never = true;
+
+}
+
+/**
+ * Void 타입
+ * void타입은 타입 계층의 중간에 위치한 평범한? 타입이다.
+ */
+
+function voidExam(){
+    function voidFunc(): void {
+        console.log("hi");
+    }
+
+
+
+};
